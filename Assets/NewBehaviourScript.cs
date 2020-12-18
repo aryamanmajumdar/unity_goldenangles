914c763bef546b28a293e4c2a1b5631ce1ee9351
aryamanmajumdar <aryaman.cis@gmail.com>
2020-12-19 04:46:33 +0530

    FIrst commit
    Added Unity project

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public GameObject sphere;
+
+    float theta = 0;
+    float b = (float)(Mathf.PI * (3.0 - Mathf.Sqrt(5.0f)));
+    float n = 1;
+    double xpos;
+    double ypos;
+    int initialradius = 10;
+    float counter = 10; //counter starts as initial radius
+
+    float R = 50; //Maximum we're allowed the bloom to grow
+
+    float increment = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //if counter is less than a certain radius,
+
+        if (counter < R)
+        {
+            //Make a ring every update
+            CreateGoldenRing(counter);
+            //increment counter
+            counter += increment;
+        }
+        else 
+        {
+            //Destroy all instantiated objects
+            DeleteAll();
+            counter = initialradius;
+            n++;
+        }
+
+    }
+
+    void Increase(float angleIncrease)
+    {
+        this.theta = this.theta + angleIncrease;
+    }
+
+    void CreateGoldenRing(float c)
+    {
+        //draw one sphere at c from center and angle theta (maybe conv coords to cart)
+        Instantiate(sphere, new Vector3(R - c * Mathf.Sin(theta), R - c * Mathf.Cos(theta), 0), Quaternion.identity);
+        //increase theta
+        Increase(n * b);
+    }
+
+    public void DeleteAll()
+    {
+        foreach (GameObject o in Object.FindObjectsOfType<GameObject>())
+        {
+            if (o.GetComponent("SphereCollider") != null) 
+            {
+                Destroy(o);
+            }
+
+        }
+    }
+}
---
+++ Assets/camera_movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_movement : MonoBehaviour
+{
+
+    public float rotationSpeed = 500000;
+    public float translationSpeed = .00000000005f;
+
+
+    public float orbitSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //for rotation
+        //Vector3 rotation = transform.eulerAngles;
+
+        //rotation.y += Input.GetAxis("Fire1") * rotationSpeed * Time.deltaTime; // Standart Left-/Right Arrows and A & D Keys
+
+        //transform.eulerAngles = rotation;
+
+        ////for translation
+        //var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //transform.position += move * translationSpeed;
+
+
+        transform.RotateAround(new Vector3(50,40,0), Vector3.up, orbitSpeed * Time.deltaTime);
+    }
+}
